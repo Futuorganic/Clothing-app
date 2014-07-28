25c3bcb1293d3b7af0afa53c992751abafd5821b
Marta Cudnik-Zientara <martacudnik@gmail.com>
2014-07-28 22:09:24 +0200

    added: seeds, action products#new, routs, template for new, form partial

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.create(name:"Statement t-shirt",description:"Cotton V-neck t-shirt with a statement",category:"t-shirt",price:"23",image:"http://www.yourcoffeebreak.co.uk/wp/wp-content/uploads/statement.jpg")
+Product.create(name:"Creamy socks",description:"Wool blend fine socks",category:"homewear",price:"12",image:"http://3.bp.blogspot.com/-mO-IAFf1cpQ/U16607TkqcI/AAAAAAAAD4E/YWdtnRtGwVA/s1600/cute+socksphoto5.jpg")
+Product.create(name:"Black loopy sweater",description:"Organic cotton handknitted sweater",category:"ktnitwear",price:"40",image:"http://www.denhamthejeanmaker.com/img/cast-hand-knitted-cotton-sweater_1000x1000_8937.png")
